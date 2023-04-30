82e2f4e9185882447e938ee78947d84e227a689b
Tasnimul116 <96897983+Tasnimul116@users.noreply.github.com>
2023-05-01 02:30:41 +0600

    Initial

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      // eslint-disable-next-line no-unused-vars
+      backgroundImage: (theme) => ({
+        "hero-pattern": "url('pattern-bg.png')",
+      }),
+    },
+  },
+  plugins: [],
+}
+
